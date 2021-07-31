deac2d228ede611919b3ee038870fe2a5b4474ee
Nikola Baci <46806109+nikolabaci98@users.noreply.github.com>
2021-07-31 18:03:47 -0400

    caluclator update

--- codepath_prework_calculator/ViewController.swift
+++ codepath_prework_calculator/ViewController.swift
@@ -13,21 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    let tipPercentages = [0.15, 0.18, 0.2]
+    let locale = Locale.current
+    var currencySymbol: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        billAmountTextFilled.keyboardType = .numberPad
+        currencySymbol = locale.currencySymbol!
+        resetFields(self)
+        self.billAmountTextFilled.becomeFirstResponder()
     }
-
-    @IBAction func calculateTip(_ sender: Any) {
+    
+    @IBAction func resetFields(_ sender: Any) {
+        billAmountTextFilled.text = ""
+        tipAmountLabel.text = currencySymbol + String(format: "%.2f", 0)
+        totalLabel.text = currencySymbol + String(format: "%.2f", 0)
+    }
+    
+    
+    @IBAction func caluclateTip(_ sender: Any) {
         let bill = Double(billAmountTextFilled.text!) ?? 0
-        
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipAmountLabel.text = currencySymbol + String(format: "%.2f", locale: Locale.current, tip)
+        totalLabel.text = currencySymbol + String(format: "%.2f", locale: Locale.current, total)
     }
+    
 }
 
